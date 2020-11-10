2f95b224a670ac8b1cf709674982ff159a586a40
Adam Hall <adamhalldesigns@gmail.com>
2020-11-10 09:09:33 +0000

    adds running server

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+interface Props { name: string }
+const App: React.FC<Props> = (props) => {
+  const x = props.name.toInt
   return (
     <div className='App'>
       <header className='App-header'>
@@ -21,6 +23,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/server/app.ts
@@ -0,0 +1,5 @@
+import { example } from 'utils/types';
+
+var a: example = { str: 'hello', num: 3 };
+
+export default a;
