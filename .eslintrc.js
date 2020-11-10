9fbf2ef32a2773c347e19262c828be988134c437
Adam Hall <adamhalldesigns@gmail.com>
2020-11-10 08:17:04 +0000

    update linter config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends:  [
+        'react-app',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    rules:  {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+};
