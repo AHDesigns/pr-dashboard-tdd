4bf5923940dece0616aca5a0831db23dda91a9bb
Adam Hall <adamhalldesigns@gmail.com>
2020-11-10 09:33:57 +0000

    Revert "linter is throwing up all over itself"
    This reverts commit 7aecb26736ea82e97938feaa9f66fc9dcfd3933b.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,10 @@
 module.exports = {
-  parser:  '@typescript-eslint/parser',
-  parserOptions:  {
-    project: './tsconfig.json',
-    warnOnUnsupportedTypeScriptVersion: false,
-  },
-  extends: [
-    'react-app',
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'no-console': 'off',
-    'prettier/prettier': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    'no-var': 'off',
-    'vars-on-top': 'off',
-    'func-names': 'off',
-    '@typescript-eslint/no-use-before-define': 0,
-    'import/extensions': ['error', 'never', { json: 'always' }],
-  },
-  plugins: [
-    'prettier',
-    '@typescript-eslint',
-  ],
-  env: {
-    node: true,
-    jest: true,
-  },
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [
-          // For path aliases, add to tsconfig and eslintrc...
-          // If not a child of src, add parent folder to NODE_PATH env
-          ['utils', './src/server/utils'],
-          ['shared', './src/shared'],
-        ],
-        extensions: ['.ts', '.tsx']
-      },
-      node: {
-        extensions: ['.ts', '.tsx']
-      }
-    }
-  },
-}
+    extends:  [
+        'react-app',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    rules:  {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+};
